7bb5d06d7b3616744d34170173ef773c0535db65
D_TECH <brijeshparamar3794@gmail.com>
2025-03-24 00:33:50 +0530

    cors added

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-     'paths' => ['api/*', 'sanctum/csrf-cookie'], 
+     'paths' => ['*', 'sanctum/csrf-cookie'], 
     'allowed_methods' => ['*'],
     'allowed_origins' => ['https://playsnexus.com'],
     'allowed_origins_patterns' => [],
